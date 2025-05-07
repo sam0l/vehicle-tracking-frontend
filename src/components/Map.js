a8c5d343519ce4b29fddbcd44274b2c6d1d1934d
sam0l <samsamolmol@gmail.com>
2025-05-07 22:14:26 +0800

    fix leaflet problem

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,6 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+
+// Configure Leaflet default icon paths
+const DefaultIcon = L.icon({
+  iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
 
 const Map = () => {
   const [detections, setDetections] = useState([]);
@@ -34,7 +47,7 @@ const Map = () => {
     <MapContainer center={[0, 0]} zoom={2} style={{ height: '500px', width: '100%' }}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        attribution='© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
       {detections && detections.length > 0 && detections.map((detection) => (
         <Marker key={detection.id} position={[detection.latitude, detection.longitude]}>
